34eb6e1265ed488a9bd7e4929ce2ddedb5ae2773
ayanadak01 <ayanadak.adak@gmail.com>
2026-04-12 20:09:57 +0530

    Learned JavaScript Operators

---
+++ operators.js
@@ -0,0 +1,47 @@
+// Types of operators in JS
+
+/*
+    1. Arithmatic Operator
+    2. Comparison Operator
+    3. Logical Operator
+    4. Assignment Operator
+    5. Conditional Operator
+    6. Type Operator
+*/
+
+// Arithmatic Operator
+
+let a = 10;
+let b = 20;
+
+let c = a + b;
+console.log("Addition", c);
+
+let sub = a - c;
+console.log("Subtraction", sub);
+
+console.log(c);
+
+let mul = a * b;
+console.log("Multiplication", mul);
+
+let exp = a ** b;
+console.log("Exponential", exp);
+
+let div = a / b;
+console.log("Division", div);
+
+let mod = a % b;
+console.log("Modulous", mod);
+
+let num1 = 30;
+let num2 = 15;
+result = num1 % num2;
+
+console.log("Result", result);
+
+console.log(num1++); //Post Increment
+
+console.log(num1);
+
+console.log(++num1); //Pre Increment
---
+++ operators2.js
@@ -0,0 +1,27 @@
+// Assignment Operators
+
+let x = 10;
+let y = 20;
+
+// x = x + y;
+x += y;
+x -= y;
+x *= y;
+x /= y;
+x %= y;
+x **= y;
+
+console.log("Result", x);
+
+let firstName = "ayan";
+let lastName = "adak";
+firstName += lastName;
+
+console.log("Full Name", firstName);
+
+const age = 20;
+const string = "my age is ";
+
+const statement = string + age;
+
+console.log("Statement", statement);
